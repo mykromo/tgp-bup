49149bfd4c4677ce5ca5058f485733be74b11c20
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-03-29 12:55:20 +0800

    add calendar

---
+++ humhub/config/messages/en-US/CalendarModule.config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Here you can configure default settings for new calendar events. These settings can be overwritten on space/profile level.' => 'Here you can configure default settings for new calendar events. These settings can be overwritten on chapter/profile level.',
+    'Here you can configure default settings for the full calendar. These settings can be overwritten on space/profile level.' => 'Here you can configure default settings for the full calendar. These settings can be overwritten on chapter/profile level.',
+];
