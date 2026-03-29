20e953c33c1bbce2cfca40582e0a9b6233d03bd4
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-03-29 10:01:36 +0800

    change calendar spaces

---
+++ humhub/plugins/chapter-label/messages/en-US/CalendarModule.notifications_views_CanceledEvent.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    '{displayName} canceled the event "{contentTitle}" in the space {spaceName}.' => '{displayName} canceled the event "{contentTitle}" in the chapter {spaceName}.',
+    '{displayName} updated the event "{contentTitle}" in the space {spaceName}.' => '{displayName} updated the event "{contentTitle}" in the chapter {spaceName}.',
+    '{displayName} reopened the event "{contentTitle}" in the space {spaceName}.' => '{displayName} reopened the event "{contentTitle}" in the chapter {spaceName}.',
+];
