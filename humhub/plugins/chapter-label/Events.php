b34475612c347410fb44fe43fbfc55349c6a71b9
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-03-29 12:25:37 +0800

    update label plugin

--- humhub/plugins/chapter-label/Events.php
+++ humhub/plugins/chapter-label/Events.php
@@ -15,7 +15,34 @@ class Events extends BaseObject
             return;
         }
 
-        // Point message overwrites to this module's messages directory
         Yii::$app->i18n->messageOverwritePath = $module->getBasePath() . '/messages';
+        Yii::$app->i18n->beforeTranslateCallback = [static::class, 'rewrite'];
+    }
+
+    public static function rewrite($category, $message, $params, $language)
+    {
+        if (stripos($message, 'space') === false) {
+            return [$category, $message, $params, $language];
+        }
+        if (strpos($language, 'en') !== 0) {
+            return [$category, $message, $params, $language];
+        }
+
+        static $replacements = [
+            ['Spaces', 'Chapters'],
+            ['spaces', 'chapters'],
+            ['SPACES', 'CHAPTERS'],
+            ['Space',  'Chapter'],
+            ['space',  'chapter'],
+            ['SPACE',  'CHAPTER'],
+        ];
+
+        $out = $message;
+        foreach ($replacements as [$s, $r]) {
+            if (strpos($out, $s) !== false) {
+                $out = str_replace($s, $r, $out);
+            }
+        }
+        return [$category, $out, $params, $language];
     }
 }
