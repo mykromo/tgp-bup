932c9f14216bd625b6552b90d5c6a79978da4605
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-03-30 15:03:03 +0800

    add misssing event

---
+++ humhub/plugins/reactions/Events.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace humhub\modules\reactions;
+
+use humhub\components\behaviors\PolymorphicRelation;
+use humhub\modules\reactions\models\Reaction;
+use humhub\modules\reactions\widgets\ReactionLink;
+use Yii;
+use yii\base\BaseObject;
+use yii\base\Event;
+
+class Events extends BaseObject
+{
+    public static function onWallEntryLinksInit(Event $event)
+    {
+        if (!Yii::$app->getModule('reactions')) {
+            return;
+        }
+        $event->sender->removeWidget('humhub\modules\like\widgets\LikeLink');
+        $object = $event->sender->object;
+        if ($object) {
+            $event->sender->addWidget(ReactionLink::class, ['object' => $object], ['sortOrder' => 10]);
+        }
+    }
+
+    public static function onCommentEntryLinksInit(Event $event)
+    {
+        if (!Yii::$app->getModule('reactions')) {
+            return;
+        }
+        $event->sender->removeWidget('humhub\modules\like\widgets\LikeLink');
+        $object = $event->sender->object;
+        if ($object) {
+            $event->sender->addWidget(ReactionLink::class, ['object' => $object], ['sortOrder' => 200]);
+        }
+    }
+
+    public static function onConversationEntryMenuInit(Event $event)
+    {
+    }
+
+    public static function onActiveRecordDelete($event)
+    {
+        $record = $event->sender;
+        if ($record->hasAttribute('id')) {
+            Reaction::deleteAll([
+                'object_id' => $record->id,
+                'object_model' => PolymorphicRelation::getObjectModel($record),
+            ]);
+        }
+    }
+
+    public static function onUserDelete($event)
+    {
+        Reaction::deleteAll(['created_by' => $event->sender->id]);
+    }
+}
