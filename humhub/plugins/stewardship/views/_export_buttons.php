d8adb7f27131811c68aad48fa201602591e45d6f
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-04-02 00:04:22 +0800

    fix download button

--- humhub/plugins/stewardship/views/_export_buttons.php
+++ humhub/plugins/stewardship/views/_export_buttons.php
@@ -1,22 +1,24 @@
 <?php
 /* @var $url string base export URL */
 $sep = (strpos($url, '?') !== false) ? '&' : '?';
-$uid = 'exp' . crc32($url);
+$baseUrl = $url . $sep . 'format=';
 ?>
 <span style="display:inline-flex; align-items:center; gap:6px">
     <span style="font-size:13px"><?= Yii::t('StewardshipModule.base', 'Export as') ?></span>
-    <select id="<?= $uid ?>" class="form-control input-sm" style="width:auto">
+    <select class="form-control input-sm export-format-select" style="width:auto">
         <option value="csv">CSV</option>
         <option value="xlsx">XLSX</option>
         <option value="html">HTML</option>
     </select>
-    <button type="button" class="btn btn-default btn-sm" id="<?= $uid ?>btn">
+    <a href="<?= $baseUrl ?>csv" class="btn btn-default btn-sm export-download-btn" data-pjax-prevent data-base-url="<?= $baseUrl ?>">
         <i class="fa fa-download"></i> <?= Yii::t('StewardshipModule.base', 'Download') ?>
-    </button>
+    </a>
 </span>
-<script>
-document.getElementById('<?= $uid ?>btn').addEventListener('click', function() {
-    var fmt = document.getElementById('<?= $uid ?>').value;
-    window.open('<?= $url . $sep ?>format=' + fmt, '_blank');
+<?php
+$this->registerJs("
+jQuery(document).on('change', '.export-format-select', function() {
+    var btn = jQuery(this).siblings('.export-download-btn');
+    btn.attr('href', btn.data('base-url') + jQuery(this).val());
 });
-</script>
+", \yii\web\View::POS_READY, 'export-format-js');
+?>
