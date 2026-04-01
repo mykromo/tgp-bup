b212183743a8c5eae1723f6bcd7b30f6d1c1207e
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-04-02 00:01:55 +0800

    change download

--- humhub/plugins/stewardship/views/_export_buttons.php
+++ humhub/plugins/stewardship/views/_export_buttons.php
@@ -1,15 +1,22 @@
 <?php
 /* @var $url string base export URL */
 $sep = (strpos($url, '?') !== false) ? '&' : '?';
-$uid = 'exp-' . md5($url);
+$uid = 'exp' . crc32($url);
 ?>
-<form class="form-inline" style="display:inline" action="" method="get" id="<?= $uid ?>">
-    <select class="form-control input-sm" id="<?= $uid ?>-fmt" style="width:auto">
+<span style="display:inline-flex; align-items:center; gap:6px">
+    <span style="font-size:13px"><?= Yii::t('StewardshipModule.base', 'Export as') ?></span>
+    <select id="<?= $uid ?>" class="form-control input-sm" style="width:auto">
         <option value="csv">CSV</option>
         <option value="xlsx">XLSX</option>
         <option value="html">HTML</option>
     </select>
-    <a href="#" class="btn btn-default btn-sm" onclick="window.location='<?= $url . $sep ?>format='+document.getElementById('<?= $uid ?>-fmt').value;return false;">
+    <button type="button" class="btn btn-default btn-sm" id="<?= $uid ?>btn">
         <i class="fa fa-download"></i> <?= Yii::t('StewardshipModule.base', 'Download') ?>
-    </a>
-</form>
+    </button>
+</span>
+<script>
+document.getElementById('<?= $uid ?>btn').addEventListener('click', function() {
+    var fmt = document.getElementById('<?= $uid ?>').value;
+    window.open('<?= $url . $sep ?>format=' + fmt, '_blank');
+});
+</script>
