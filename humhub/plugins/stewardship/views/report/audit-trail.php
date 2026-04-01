f44a1383a6884310d8e7752c678dc914ad12d450
CBS\michael.limpios <michael.limpios@ctos.com.my>
2026-04-01 23:09:23 +0800

    fix amount

--- humhub/plugins/stewardship/views/report/audit-trail.php
+++ humhub/plugins/stewardship/views/report/audit-trail.php
@@ -20,7 +20,12 @@ $cc = $contentContainer;
 <td style="white-space:nowrap"><?= Yii::$app->formatter->asDatetime($log->created_at) ?></td>
 <td><?php if ($log->user): ?><a href="<?= $log->user->getUrl() ?>"><?= Html::encode($log->user->displayName) ?></a><?php else: ?>System<?php endif; ?></td>
 <td><span class="label label-info"><?= ucfirst($log->action) ?></span> <?= Html::encode($log->entity_type) ?> #<?= $log->entity_id ?></td>
-<td><?php if ($log->field_changed): ?><strong><?= Html::encode($log->field_changed) ?></strong>: <?php if ($log->old_value): ?><s class="text-muted"><?= Html::encode($log->old_value) ?></s> &rarr; <?php endif; ?><?= Html::encode($log->new_value) ?><?php endif; ?></td>
+<td><?php if ($log->field_changed):
+    $amountFields = ['amount', 'balance', 'amount_awarded', 'amount_spent'];
+    $isAmount = in_array($log->field_changed, $amountFields);
+    $oldVal = $isAmount && $log->old_value !== null ? \humhub\modules\stewardship\helpers\Currency::format($log->old_value) : Html::encode($log->old_value);
+    $newVal = $isAmount && $log->new_value !== null ? \humhub\modules\stewardship\helpers\Currency::format($log->new_value) : Html::encode($log->new_value);
+?><strong><?= Html::encode($log->field_changed) ?></strong>: <?php if ($log->old_value): ?><s class="text-muted"><?= $oldVal ?></s> &rarr; <?php endif; ?><?= $newVal ?><?php endif; ?></td>
 </tr>
 <?php endforeach; ?>
 </tbody></table></div>
